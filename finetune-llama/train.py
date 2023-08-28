8b5bc75ed51367b5ee5995a46b0a046734a4fb28
jackycamp <jackcampanella08@gmail.com>
2023-08-27 20:30:21 -0400

    finetune-llama

---
+++ finetune-llama/train.py
@@ -0,0 +1,15 @@
+# referenced from https://blog.ovhcloud.com/fine-tuning-llama-2-models-using-a-single-gpu-qlora-and-ai-notebooks/
+# with a few changes/minimizations
+
+def train(model, tokenizer, dataset, outdir):
+     # enable gradient checkpointing to reduce memory usage during fine-tuning
+
+
+    pass
+    
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
